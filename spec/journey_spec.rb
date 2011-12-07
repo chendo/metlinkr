d143b4a4ddc3d9234d9fa19abbf4558eb9dbff90
Jack Chen (chendo) <me@chen.do>
2011-12-07 22:12:53 +1100

    Added some tests

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -12,6 +12,14 @@ describe Metlinkr::Journey do
   it "parses journey effectively" do
     subject.steps.length.should == 7
 
-    pp subject.steps
+    step = subject.steps.first
+
+    step.method.should == :tram
+    step.origin.should == "Stop 29 - Barkers Rd/High St (Kew)"
+    step.destination.should == "Stop 19 - North Richmond Railway Station/Victoria St (Richmond)"
+    step.departure_time.should == "8:39pm"
+    step.arrival_time.should == "8:48pm"
+    step.duration.should == "9 min"
+    step.route.should == "109 tram towards Port Melbourne"
   end
 end
